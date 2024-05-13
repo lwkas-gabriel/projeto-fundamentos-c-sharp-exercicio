66d12d843fc5703e7897741bbc5cdae5c068ad22
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-05-13 19:14:44 -0300

    feat: exercicios 1 e 2

--- ExerciciosRockeatseat/Program.cs
+++ ExerciciosRockeatseat/Program.cs
@@ -1,10 +1,30 @@
 ﻿
+using System.Text;
+
 namespace ExerciciosRocketseat
 {
     class Program
     {
         public static void Main()
         {
+            Exercicio2();
+        }
+
+        public static void Exercicio1()
+        {
+            Console.WriteLine("Digite seu nome:");
+            string seuNome = Console.ReadLine();
+            Console.WriteLine($"Olá, {seuNome}! Seja muito bem-vindo!");
+        }
+
+        public static void Exercicio2()
+        {
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite seu sobrenome:");
+            string sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"{nome} {sobrenome}");
 
         }
     }
